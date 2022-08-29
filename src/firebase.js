21598cf3ab4c70ee085ad08e4ddc04eb189db300
SimonOConnor28 <simonoconnor28@gmail.com>
2022-08-29 14:19:30 -0400

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDQGZqtPQL0vzgQwflnF76ZPSRRUBIT6PQ",
+  authDomain: "binge-fest-9c84a.firebaseapp.com",
+  projectId: "binge-fest-9c84a",
+  storageBucket: "binge-fest-9c84a.appspot.com",
+  messagingSenderId: "383355556791",
+  appId: "1:383355556791:web:ad26fe330433a4891a4e32",
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
